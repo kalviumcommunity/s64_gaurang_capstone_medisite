fa1b0da887c8efd6ca44822f542ff99ab972e9c7
gastrotope427 <gaurang.mishra.s64@kalvium.community>
2025-04-11 14:48:47 +0530

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
+require('dotenv').config(); // Load environment variables from .env
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT;
 const HOST = 'localhost';
 
 
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
 });
-
 
 app.listen(PORT, HOST, () => {
   console.log(`Server is running at http://${HOST}:${PORT}`);
